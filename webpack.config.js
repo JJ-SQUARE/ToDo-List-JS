2add2fb4b297406907985836ec309e5894a86b6b
Juan <jjhuertapsic@gmail.com>
2022-04-10 11:51:32 -0500

    thirdCommit

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
         }),
         new CopyPlugin({
             patterns: [
-              { from: "src/assets/", to: "assests/" },
+              { from: "src/assets/", to: "assets/" },
             ]
         }),
     ]
